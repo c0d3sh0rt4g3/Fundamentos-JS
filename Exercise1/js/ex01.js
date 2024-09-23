79ef831beb0ccf75b5800608b2c2790c370d7d01
out0f4mm0 <eriking2004@gmail.com>
2024-09-23 10:54:57 +0200

    Ended second part of exercise 1

--- Exercise1/js/ex01.js
+++ Exercise1/js/ex01.js
@@ -1,6 +1,14 @@
-const exampleArray = [23, 34, 67, 2]
-const exampleBoolean = true
-const exampleNumber = 2
+let exampleArray = [23, 34, 67, 2]
+let exampleBoolean = true
+let exampleNumber = 2
+
+console.log(typeof exampleArray)
+console.log(typeof exampleBoolean)
+console.log(typeof exampleNumber)
+
+exampleArray = String(exampleArray)
+exampleBoolean = String(exampleBoolean)
+exampleNumber = String(exampleNumber)
 
 console.log(typeof exampleArray)
 console.log(typeof exampleBoolean)
