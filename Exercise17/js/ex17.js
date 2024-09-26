4eec238a6550e886cc07f2ff34094b161e2f7ca3
out0f4mm0 <eriking2004@gmail.com>
2024-09-26 13:18:50 +0200

    Exercise 17 done

---
+++ Exercise17/js/ex17.js
@@ -0,0 +1,15 @@
+let i = 0
+let timesIntroduced18 = 0
+
+while (i < 5){
+    const givenAge = Number(prompt("What is your age?"))
+    if (isNaN(givenAge) || givenAge % 1 !==0 || givenAge < 0){
+        alert("Please enter a valid age")
+    }else if (givenAge >= 18){
+        timesIntroduced18++
+        console.log(timesIntroduced18)
+    }
+    i++
+}
+
+alert(`There's been introduced an age higher or equal to 18 ${timesIntroduced18} times`)
