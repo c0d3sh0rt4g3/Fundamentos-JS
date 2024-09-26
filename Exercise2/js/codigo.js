136e3feceeca991ae69eb1842223e224625ee587
out0f4mm0 <eriking2004@gmail.com>
2024-09-26 10:59:18 +0200

    Exercise 2 done

---
+++ Exercise2/js/codigo.js
@@ -0,0 +1,4 @@
+// Añadimos un console log para mostrar "Hola Mundo!"
+console.log("Hola Mundo!")
+// Añadimos el segundo console log para mostrar "Soy el primer script"
+console.log("Soy el primer script")
