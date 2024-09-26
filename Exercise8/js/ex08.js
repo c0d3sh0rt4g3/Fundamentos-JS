7f01fa351b7f13a0477144b09d001a818ebba595
out0f4mm0 <eriking2004@gmail.com>
2024-09-26 12:11:21 +0200

    Fixed typo on ex08.js comment

--- Exercise8/js/ex08.js
+++ Exercise8/js/ex08.js
@@ -3,7 +3,7 @@ const givenAge = Number(prompt("Whats your age?"))
 // We check that the data given is a number, that it's higher than 0, that it's lower than 100 and that it isn't a float number
 if (isNaN(givenAge) || givenAge < 0 || givenAge > 100 || givenAge % 1 !== 0) {
     alert("Please enter a valid age")
-// If the age given is between 18 and 15 years old it would pop an alert
+// If the age given is between 18 and 25 years old it would pop an alert
 } else if (givenAge >= 18 && givenAge <= 25) {
     alert("You have an age between 18 and 25 years old")
 }
