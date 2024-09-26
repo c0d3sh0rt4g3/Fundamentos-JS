061f634dffc99a06c1f5e6735a393d5f2e6d101b
out0f4mm0 <eriking2004@gmail.com>
2024-09-26 11:17:15 +0200

    Exercise 4 done

---
+++ Exercise3/js/ex04.js
@@ -0,0 +1,7 @@
+let numString = "420"
+let num = 180
+
+let sum = numString + num
+
+// Mostramos la suma del string y el numero, el resultado será 420180 ya que al ser un string un numero no se suman sus valores sino ambas cadenas
+console.log(sum)
