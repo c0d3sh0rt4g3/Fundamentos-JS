36e3b61b049dfa957be3648cd0a1979de4dc62de
out0f4mm0 <eriking2004@gmail.com>
2024-09-26 12:52:30 +0200

    Exercise 15 done

---
+++ Exercise15/js/ex15.js
@@ -0,0 +1,18 @@
+let num1 = 5
+let num2 = 8
+
+if(num1 < num2){
+    alert("number 1 isn't bigger than number 2")
+}
+
+if (num2 > 0){
+    alert("number 2 is positive")
+}
+
+if (num1 < 0 || num1 !== 0){
+    alert("number 1 is negative or different from 0")
+}
+
+if (num1 + 1 < num2){
+    alert("Adding 1 to number 1 doesn't make it bigger or equal than number 2")
+}
