6bfb72250c7ba3e22130a86338cbd7e84e1043e6
out0f4mm0 <eriking2004@gmail.com>
2024-09-26 11:06:57 +0200

    Exercise 3 done

---
+++ Exercise3/js/ex03.js
@@ -0,0 +1,15 @@
+let num1 = 5
+let num2 = 2
+
+console.log(`${num1} + ${num2} = ${num1 + num2}`)
+console.log(`${num1} - ${num2} = ${num1 - num2}`)
+console.log(`${num1} x ${num2} = ${num1 * num2}`)
+console.log(`${num1} / ${num2} = ${num1 / num2}`)
+
+// Utilizamos un operador de decremento en num1
+-- num1
+// Utilizamos un operador de incremento en num2
+++ num2
+
+console.log(num1)
+console.log(num2)
